844d7df28160f0a675a68f0c46e3b3ea38d474b6
Lincoln-Andrade-Silva <lincolnandradesilva@outlook.com>
2022-03-21 19:57:04 -0300

    criaçao-components/rotas

--- myfilms/src/app/app.module.ts
+++ myfilms/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AdminComponent } from './admin/admin.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { NavComponent } from './nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AdminComponent,
+    SobreComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,

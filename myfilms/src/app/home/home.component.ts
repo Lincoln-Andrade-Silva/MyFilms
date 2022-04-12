121de77093528697ee5a5fcb5ee5cf56d8da8524
Lincoln Andrade Silva <Lincoln.Silva1@br.ey.com>
2022-04-12 09:34:29 -0300

    home

--- myfilms/src/app/home/home.component.ts
+++ myfilms/src/app/home/home.component.ts
@@ -9,18 +9,33 @@ import { Filme } from 'src/models/Filme';
 })
 export class HomeComponent implements OnInit {
 
-  filmes : Filme[];
+  filmes: Filme[];
 
-  constructor(private service : FilmesService) {
+  constructor(private service: FilmesService) {
     this.filmes = [];
-   }
 
-  ngOnInit(): void {  
+  }
 
+  get groupedFilmes() {
+    return Object.entries(
+      this.filmes.reduce((filmes, filme) => {
+        const categoria = filme.categoria;
+        filmes[categoria] = filmes[categoria] ? [...filmes[categoria], filme] : [filme];
+        return filmes
+      }, {} as { [key: string]: Filme[] })
+    )
+  }
+
+  ngOnInit(): void {
     this.service.listar().subscribe(filmes => {
       console.log(filmes);
       this.filmes = filmes;
     });
   }
-}
 
+  truncate(str: string | undefined) {
+    if (str) {
+      return str.length > 20 ? str.substring(0, 160) + '...' : str;
+    } else { return }
+  }
+}
--- myfilms/src/services/filmes.service.ts
+++ myfilms/src/services/filmes.service.ts
@@ -8,43 +8,32 @@ import { Filme } from 'src/models/Filme';
 })
 export class FilmesService {
 
-  filmes : Filme[];
+  filmes: Filme[];
 
 
   URL = "http://localhost:3001/filmes";
 
   constructor(private http: HttpClient) {
     this.filmes = [];
-  } 
+  }
 
-  buscarPorId(id: string): Observable<Filme>{
+  buscarPorId(id: string): Observable<Filme> {
     return this.http.get<Filme>(this.URL + "/" + id);
   }
 
-  listar(): Observable<Filme[]>{
+  listar(): Observable<Filme[]> {
     return this.http.get<Filme[]>(this.URL);
   }
 
-  incluir(filme : Filme): Observable<any>{
+  incluir(filme: Filme): Observable<any> {
     return this.http.post<any>(this.URL, filme);
   }
 
-  atualizar(id: number, filme: Filme) : Observable<any> {
+  atualizar(id: number, filme: Filme): Observable<any> {
     return this.http.put<any>(this.URL + "/" + id, filme);
   }
 
-  excluir(id : number) : Observable<any>{
+  excluir(id: number): Observable<any> {
     return this.http.delete<any>(this.URL + "/" + id);
   }
-
-  get groupedFilmes() {
-    return Object.entries(
-      this.filmes.reduce((filmes, filme) => {
-          const categoria = filme.categoria;
-          filmes[categoria] = filmes[categoria] ? [...filmes[categoria], filme] : [filme];
-          return filmes
-      }, {} as { [key: string]: Filme[] })
-  )
-  }
-
 }

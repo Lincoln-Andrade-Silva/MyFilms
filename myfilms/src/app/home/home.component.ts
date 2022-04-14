cf28a6d6893fc8137ae7b268ee9db930575d401e
Lincoln Andrade Silva <Lincoln.Silva1@br.ey.com>
2022-04-14 11:52:41 -0300

    groupby no console log

--- myfilms/src/app/home/home.component.ts
+++ myfilms/src/app/home/home.component.ts
@@ -17,13 +17,37 @@ export class HomeComponent implements OnInit {
     this.filmes = [];
   }
 
+  group_by(lista: any[], coluna: string) {
+    var colunas: any = {};
+    var resultado: any = [];
+
+    lista.forEach(function (item) {
+      var reg: any = {};
+
+      colunas[item[coluna]] = colunas[item[coluna]] || [];
+
+      for (var i in item)
+        if (i != coluna)
+          reg[i] = item[i];
+
+      colunas[item[coluna]].push(reg);
+    });
+
+    for (var i in colunas)
+      resultado.push({ key: i, values: colunas[i] });
+
+    return resultado;
+  }
+  
   ngOnInit(): void {
     this.service.listar()
       .subscribe(filmes => {
-        this.filmes = filmes
+        this.filmes = filmes;
+        filmes =  this.group_by(filmes, "categoria")
+        console.log(filmes)
       })
   }
- 
+
   truncate(str: string | undefined) {
     if (str) {
       return str.length > 20 ? str.substring(0, 200) + '...' : str;
